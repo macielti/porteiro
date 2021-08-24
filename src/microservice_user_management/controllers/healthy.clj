3f860ada64a190982ecfc879aab1eebf3da98e5e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-08-23 21:21:16 -0300

    add healthy check endpoint

---
+++ src/microservice_user_management/controllers/healthy.clj
@@ -0,0 +1,15 @@
+(ns microservice-user-management.controllers.healthy
+  (:require [microservice-user-management.db.datomic.user :as datomic.user]))
+
+(defn healthy-check
+  [datomic config]
+  (let [dependencies {:components [{:component  :config
+                                    :is-healthy (try (:jw-token-secret config)
+                                                     true
+                                                     (catch Exception _ false))}
+                                   {:name       :datomic
+                                    :is-healthy (try (datomic.user/by-username "" datomic)
+                                                     true
+                                                     (catch Exception _ false))}]}]
+    (assoc dependencies :is-healthy (every? :is-healthy
+                                            (:components dependencies)))))
--- src/microservice_user_management/diplomatic/http_in.clj
+++ src/microservice_user_management/diplomatic/http_in.clj
@@ -2,7 +2,8 @@
   (:use [clojure pprint])
   (:require [schema.core :as s]
             [microservice-user-management.controllers.user :as controllers.user]
-            [microservice-user-management.controllers.auth :as controllers.auth]))
+            [microservice-user-management.controllers.auth :as controllers.auth]
+            [microservice-user-management.controllers.healthy :as controllers.healthy]))
 
 (s/defn create-user!
   [{user              :json-params
@@ -13,3 +14,8 @@
   [{auth                     :json-params
     {:keys [datomic config]} :components}]
   {:status 200 :body (controllers.auth/auth auth config datomic)})
+
+(s/defn healthy-check
+  [{{:keys [datomic config]} :components}]
+  (let [check-result (controllers.healthy/healthy-check datomic config)]
+    {:status (if (:is-healthy check-result) 200 503) :body check-result}))

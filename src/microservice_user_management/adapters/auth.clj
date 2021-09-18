0a9d68f44b5bb061f38c4755ac7db310a19ff53e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-09-17 21:56:38 -0300

    move jwt secret to datomic for each individual user

--- src/microservice_user_management/adapters/auth.clj
+++ src/microservice_user_management/adapters/auth.clj
@@ -1,11 +1,13 @@
 (ns microservice-user-management.adapters.auth
+  (:use [clojure pprint])
   (:require [schema.core :as s]
             [microservice-user-management.wire.in.auth :as wire.in.auth]
             [humanize.schema :as h]
             [clojure.string :as str]
-            [buddy.sign.jwt :as jwt])
+            [buddy.sign.jwt :as jwt]
+            [cheshire.core :as json])
   (:import (clojure.lang ExceptionInfo)
-           (java.util UUID)))
+           (java.util UUID Base64)))
 
 (s/defn wire->internal :- wire.in.auth/Auth
   [auth :- wire.in.auth/Auth]
@@ -19,11 +21,18 @@
                          :cause  (get-in (h/ex->err e) [:unknown :error])}))))))
 
 (defn jwt-wire->internal
-  [authorization jwt-secret]
-  (let [{:keys [id] :as user} (try (-> (str/split authorization #" ")
-                                       last
-                                       (jwt/unsign jwt-secret))
-                                   (catch ExceptionInfo _ (throw (ex-info "Invalid token"
-                                                                          {:status 422
-                                                                           :cause  "Invalid token"}))))]
-    (assoc user :id (UUID/fromString id))))
+  ([jw-token jwt-secret]
+   (let [{:keys [id] :as user} (try
+                                 (jwt/unsign jw-token jwt-secret)
+                                 (catch ExceptionInfo _ (throw (ex-info "Invalid token"
+                                                                        {:status 422
+                                                                         :cause  "Invalid token"}))))]
+     (assoc user :id (UUID/fromString id)))))
+
+(defn decoded-jwt
+  [jw-token]
+  (let [[_ payload __] (str/split jw-token #"\." 3)
+        clj-payload (-> (.decode (Base64/getDecoder) ^String payload)
+                        (String.)
+                        (json/decode true))]
+    (assoc clj-payload :id (UUID/fromString (:id clj-payload)))))
--- src/microservice_user_management/wire/datomic/session.clj
+++ src/microservice_user_management/wire/datomic/session.clj
@@ -3,26 +3,31 @@
   (:import (java.util Date)))
 
 (def session
-  [{:db/ident       :session-token/id
+  [{:db/ident       :session/id
     :db/valueType   :db.type/uuid
     :db/cardinality :db.cardinality/one
     :db/unique      :db.unique/identity
     :db/doc         "Session token entity Id"}
-   {:db/ident       :session-token/user-id
+   {:db/ident       :session/user-id
     :db/valueType   :db.type/uuid
     :db/cardinality :db.cardinality/one
     :db/doc         "User id that own this session token"}
-   {:db/ident       :session-token/secret
+   {:db/ident       :session/secret
     :db/valueType   :db.type/uuid
     :db/cardinality :db.cardinality/one
     :db/doc         "Secret to be able to validate JWTokens"}
-   {:db/ident       :session-token/created-at
+   {:db/ident       :session/valid?
+    :db/valueType   :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Instant emission of the password request"}
+   {:db/ident       :session/created-at
     :db/valueType   :db.type/instant
     :db/cardinality :db.cardinality/one
     :db/doc         "Instant emission of the password request"}])
 
 (s/defschema Session
-  {:session-token/id         s/Uuid
-   :session-token/user-id    s/Uuid
-   :session-token/secret     s/Uuid
-   :session-token/created-at Date})
+  {:session/id         s/Uuid
+   :session/user-id    s/Uuid
+   :session/secret     s/Uuid
+   :session/valid?     s/Bool
+   :session/created-at Date})

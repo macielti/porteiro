34d43f60fa1e0675e0ea7f5dded76260cf2ca5e2
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-02-03 08:56:17 -0300

    add endpoint

---
+++ src/porteiro/diplomatic/http_server/contact.clj
@@ -0,0 +1,9 @@
+(ns porteiro.diplomatic.http-server.contact
+  (:require [schema.core :as s]
+            [porteiro.controllers.contact :as controllers.contact]))
+
+(s/defn fetch-contacts
+  [{{:keys [id]}      :user-identity
+    {:keys [datomic]} :components}]
+  {:status 200
+   :body   (controllers.contact/fetch-contacts id datomic)})

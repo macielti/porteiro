5e7d38729500bfc712d00e84faddc49f63699b29
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-03-01 15:28:30 -0300

    add interceptor for email already in use and test

--- src/porteiro/interceptors/user.clj
+++ src/porteiro/interceptors/user.clj
@@ -3,13 +3,25 @@
             [common-clj.error.core :as common-error]))
 
 (def username-already-in-use-interceptor
-  {:name  ::user-already-in-use-interceptor
+  {:name  ::username-already-in-use-interceptor
    :enter (fn [{{{:keys [username] :or {username ""}} :json-params
                  {:keys [datomic]}                    :components} :request :as context}]
             (let [user (datomic.user/by-username username (:connection datomic))]
               (when-not (empty? user)
                 (common-error/http-friendly-exception 409
-                                                      "not-unique-user"
+                                                      "not-unique"
                                                       "Username already in use"
                                                       "username already in use by other user")))
+            context)})
+
+(def email-already-in-use-interceptor
+  {:name  ::email-already-in-use-interceptor
+   :enter (fn [{{{:keys [email] :or {email ""}} :json-params
+                 {:keys [datomic]}              :components} :request :as context}]
+            (let [user (datomic.user/by-email email (:connection datomic))]
+              (when-not (empty? user)
+                (common-error/http-friendly-exception 409
+                                                      "not-unique"
+                                                      "Email already in use"
+                                                      "Email already in use by other user")))
             context)})

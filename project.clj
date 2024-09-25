1590493ba0b0e55030da275538b3935ccb1185e7
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-09-25 05:42:22 -0300

    remove duplicated and unused dependencies

--- project.clj
+++ project.clj
@@ -3,35 +3,23 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :plugins [[lein-cloverage "1.2.3"]
-            [lein-environ "1.2.0"]]
+  :plugins []
 
   :exclusions [log4j]
 
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ch.qos.logback/logback-classic "1.4.7"]
-                 [io.pedestal/pedestal.service-tools "0.5.10"]
                  [org.postgresql/postgresql "42.7.4"]
                  [net.clojars.macielti/common-clj "30.63.70" :exclusions [datalevin]]
                  [camel-snake-kebab "0.4.3"]
                  [danlentz/clj-uuid "0.1.9"]
                  [de.ubercode.clostache/clostache "1.4.0"]
-                 [org.apache.kafka/kafka-clients "3.4.0"]
-                 [io.pedestal/pedestal.service "0.5.10"]
-                 [com.datomic/datomic-free "0.9.5697"]
-                 [io.pedestal/pedestal.jetty "0.5.10"]
-                 [io.pedestal/pedestal.route "0.5.10"]
-                 [com.stuartsierra/component "1.1.0"]
                  [nubank/matcher-combinators "3.5.0"]
                  [fundingcircle/jackdaw "0.9.9"]
+                 [buddy/buddy-sign "3.4.333"]
                  [buddy/buddy-hashers "1.8.158"]
                  [prismatic/schema "1.4.1"]
-                 [camel-snake-kebab "0.4.3"]
-                 [danlentz/clj-uuid "0.1.9"]
-                 [buddy/buddy-sign "3.4.333"]
                  [siili/humanize "0.1.1"]
-                 [cheshire "5.11.0"]
-                 [environ "1.2.0"]]
+                 [cheshire "5.11.0"]]
 
   :injections [(require 'hashp.core)]
 
